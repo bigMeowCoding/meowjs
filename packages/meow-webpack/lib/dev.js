ec2b7e382fa4aa96561eb0fe2309147c53c94240
zhouyijun <zhou495248579@hotmail.com>
2022-07-20 23:44:52 +0800

    feat:dev server base test

--- packages/meow-webpack/lib/dev.js
+++ packages/meow-webpack/lib/dev.js
@@ -5,6 +5,76 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.dev = dev;
 
+var _webpack = _interopRequireDefault(require("webpack"));
+
+var _WebpackDevServerUtils = require("react-dev-utils/WebpackDevServerUtils");
+
+var _webpackDevServer = _interopRequireDefault(require("webpack-dev-server"));
+
+var _chalk = _interopRequireDefault(require("chalk"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const isInteractive = process.stdout.isTTY;
+const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 8000;
+const HOST = process.env.HOST || "0.0.0.0";
+const PROTOCOL = process.env.HTTPS ? "https" : "http";
+
+const noop = () => {};
+
+const port = DEFAULT_PORT;
+process.env.NODE_ENV = "development";
+
 function dev() {
   console.log("meow meow meow");
+  const urls = (0, _WebpackDevServerUtils.prepareUrls)(PROTOCOL, HOST, port);
+  const compiler = (0, _WebpackDevServerUtils.createCompiler)(_webpack.default, {
+    entry: "./src/index.js"
+  }, "Your App", urls);
+  console.log(compiler);
+  const serverConfig = {
+    disableHostCheck: true,
+    compress: true,
+    clientLogLevel: 'none',
+    hot: true,
+    quiet: true,
+    headers: {
+      'access-control-allow-origin': '*'
+    },
+    publicPath: 'build',
+    watchOptions: {
+      ignored: /node_modules/
+    },
+    overlay: false,
+    host: HOST,
+    https: !!process.env.HTTPS,
+
+    // contentBase: contentBase || process.env.CONTENT_BASE,
+    before(app) {
+      console.log('before');
+    },
+
+    after(app) {
+      console.log('after');
+    }
+
+  };
+  const devServer = new _webpackDevServer.default(compiler, serverConfig);
+  devServer.listen(port, HOST, err => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    if (isInteractive) {// clearConsole();
+    }
+
+    console.log(_chalk.default.cyan('\nStarting the development server...\n')); // if (openBrowserOpts) {
+    //   openBrowser(urls.localUrlForBrowser);
+    // }
+    // send({ type: STARTING });
+    // if (afterServer) {
+    //   afterServer(devServer);
+    // }
+  });
 }
--- packages/meow-webpack/src/dev.js
+++ packages/meow-webpack/src/dev.js
@@ -1,3 +1,72 @@
+import webpack from "webpack";
+import {
+  createCompiler,
+  prepareUrls,
+} from "react-dev-utils/WebpackDevServerUtils";
+import WebpackDevServer from 'webpack-dev-server';
+import chalk from 'chalk';
+
+const isInteractive = process.stdout.isTTY;
+const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 8000;
+const HOST = process.env.HOST || "0.0.0.0";
+const PROTOCOL = process.env.HTTPS ? "https" : "http";
+const noop = () => {};
+const port = DEFAULT_PORT
+
+process.env.NODE_ENV = "development";
 export function dev() {
   console.log("meow meow meow");
+  const urls = prepareUrls(PROTOCOL, HOST, port);
+  const compiler = createCompiler(
+    webpack,
+    {
+      entry: "./src/index.js",
+    },
+    "Your App",
+    urls
+  );
+  console.log(compiler);
+  const serverConfig = {
+    disableHostCheck: true,
+    compress: true,
+    clientLogLevel: 'none',
+    hot: true,
+    quiet: true,
+    headers: {
+      'access-control-allow-origin': '*',
+    },
+    publicPath: 'build',
+    watchOptions: {
+      ignored: /node_modules/,
+    },
+    overlay: false,
+    host: HOST,
+    https: !!process.env.HTTPS,
+    // contentBase: contentBase || process.env.CONTENT_BASE,
+    before(app) {
+      console.log('before')
+    },
+    after(app) {
+      console.log('after')
+    },
+  };
+
+  const devServer = new WebpackDevServer(compiler, serverConfig);
+  devServer.listen(port, HOST, err => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    if (isInteractive) {
+      // clearConsole();
+    }
+    console.log(chalk.cyan('\nStarting the development server...\n'));
+    // if (openBrowserOpts) {
+    //   openBrowser(urls.localUrlForBrowser);
+    // }
+    // send({ type: STARTING });
+    // if (afterServer) {
+    //   afterServer(devServer);
+    // }
+  });
 }
